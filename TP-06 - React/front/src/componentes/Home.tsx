01f5204b2b21de2680bee73c5d5a206cac0945b7
Pablo Valentini UTN <pablogastonvalentini@gmail.com>
2022-05-14 18:15:20 -0300

    TP 6

---
+++ TP-06 - React/front/src/componentes/Home.tsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+import { getInstrumentosJSONFetch, getInstrumentosPorBusqueda } from './FuncionesApi';
+import { ItemInstrumento } from './ItemInstrumento';
+import { Navigation } from './Navigation';
+import Instrumento from './Instrumento';
+import { Alert, Button, Card, Container, Row } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
+
+
+export const Home = () => {
+    
+  const {parametro} = useParams();
+
+  const [instrumentos, setInstrumentos] = useState<Instrumento[]>([]);
+    
+    const getInstrumentos = async () => {
+      console.log("PARAMETRO " + parametro);
+      if(parametro && parametro != ""){
+        let datos:Instrumento[] = await getInstrumentosPorBusqueda(parametro);
+        setInstrumentos(datos);
+      }else{
+        let datos:Instrumento[] = await getInstrumentosJSONFetch();
+        setInstrumentos(datos);
+      }
+    }
+
+    useEffect(() => {
+      getInstrumentos();
+    }, []);
+
+    
+    return (
+        <>
+        <Navigation></Navigation>
+          <Container fluid="md">
+              <Row>  
+               {instrumentos.map((instrumento:Instrumento) => 
+                <ItemInstrumento 
+                  key={instrumento.id} 
+                  id={instrumento.id} 
+                  instrumento={instrumento.nombre}
+                  marca={instrumento.marca}
+                  modelo={instrumento.modelo}
+                  imagen={instrumento.imagen}
+                  precio={instrumento.precio} 
+                  costoEnvio={instrumento.costoEnvio}
+                  cantidadVendida={instrumento.cantidadVendida}
+                  descripcion={instrumento.descripcion}
+                  ></ItemInstrumento>
+               )}
+              </Row>
+          </Container>
+        </>
+    )
+}
+
